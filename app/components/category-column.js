cad0a520fc5c87450c80a906bb19c9bc42ff6c5b
Cole Willsea <coleww@gmail.com>
2015-03-16 09:52:47 -0700

    move answers onto category model

--- app/components/category-column.js
+++ app/components/category-column.js
@@ -2,6 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/category-column';
 
 export default Ember.Component.extend({
-  layout: layout,
-  answerValues: Ember.A([200, 400, 600, 800, 1000])
+  layout: layout
 });
--- app/models/category.js
+++ app/models/category.js
@@ -1,8 +1,20 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   displayTitle: function(){
     return this.get('title').toUpperCase();
-  }.property('title')
+  }.property('title'),
+  answers: null,
+  addAnswers: function(){
+    var answers = Ember.A([
+      Ember.Object.create({value: 200, asked: false}),
+      Ember.Object.create({value: 400, asked: false}),
+      Ember.Object.create({value: 600, asked: false}),
+      Ember.Object.create({value: 800, asked: false}),
+      Ember.Object.create({value: 1000, asked: false})
+    ]);
+    this.set('answers', answers);
+  }.on('init')
 });
