95365f43c18d6dc490a4ec0582ed7db6be0d9290
Cole Willsea <coleww@gmail.com>
2015-03-16 20:47:01 -0700

    push actions up to parent route

--- app/routes/game.js
+++ app/routes/game.js
@@ -13,6 +13,10 @@ export default Ember.Route.extend({
     endRound: function(){
       this.get('players').set('currentAnswer', null);
       this.transitionTo('game.index');
+    },
+    ask: function(category, value){
+      this.transitionTo('game.answer', category, value.get('value'));
+      this.get('players').set('currentAnswer', value);
     }
   }
 });
--- app/routes/game/index.js
+++ app/routes/game/index.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  players: Ember.inject.service('players'),
   model: function(){
     return this.modelFor('game');
-  },
-  actions: {
-    ask: function(category, value){
-      this.transitionTo('game.answer', category, value.get('value'));
-      this.get('players').set('currentAnswer', value);
-    }
   }
 });
