0e6f6c6d1d02946defcb4310a0696790eb7a55db
Cole Willsea <coleww@gmail.com>
2015-03-29 18:09:19 -0700

    interface to assign/deduct/pass from "host" device

--- app/controllers/host.js
+++ app/controllers/host.js
@@ -2,9 +2,36 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   currentClue: null,
+  players: null,
+  active: false,
   actions: {
     onmessage: function(socketEvent){
-      this.set('currentClue', JSON.parse(socketEvent.data));
+      var data = JSON.parse(socketEvent.data);
+      var players = Ember.A();
+      data.players.forEach(function(player){
+        player.active = true;
+        players.pushObject(Ember.Object.create(player));
+      });
+      this.set('players', players);
+      this.set('active', true);
+      this.set('currentClue', data.clue);
+    },
+    assign: function(player){
+      this.get('players').invoke('set', 'active', false);
+      this.set('active', false);
+      var result = {};
+      result[player.name] = true;
+      this.send('emit', {result: result}, true);
+    },
+    deduct: function(player){
+      player.set('active', false);
+      var result = {};
+      result[player.name] = false;
+      this.send('emit', {result: result}, true);
+    },
+    pass: function(){
+      this.get('players').invoke('set', 'active', false);
+      this.set('active', false);
     }
   }
 });
--- tests/acceptance/host-test.js
+++ tests/acceptance/host-test.js
@@ -18,13 +18,140 @@ module('Acceptance: Host', {
 });
 
 test('host route shows current clue', function(assert) {
+  assert.expect(2);
   visit('/host/abc123');
 
   var hostController = application.__container__.lookup('controller:host');
-  hostController.send('onmessage', {data: "{\"q\":\"foo\",\"a\":\"bar\"}"});
+  hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+
 
   andThen(function() {
     assert.equal(find('.question:contains("foo")').size(), 1);
     assert.equal(find('.answer:contains("bar")').size(), 1);
+  });
+});
+
+test('host route shows current players', function(assert) {
+  assert.expect(3);
+  visit('/host/abc123');
+  var hostController = application.__container__.lookup('controller:host');
+  hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+
+  andThen(function() {
+    assert.equal(find('.player:contains("Bob")').size(), 1);
+    assert.equal(find('.player:contains("Deb")').size(), 1);
+    assert.equal(find('.player:contains("Tim")').size(), 1);
+  });
+
+});
+
+test('assign/deduct buttons disappear after deduction', function(assert){
+  assert.expect(6);
+  visit('/host/abc123');
+  var hostController = application.__container__.lookup('controller:host');
+  var hostRoute = application.__container__.lookup('route:host');
+  hostRoute.reopen({
+    actions: {
+      emit:function(data){
+        assert.ok(!data.result.Bob);
+      }
+    }
+  });
+  andThen(function() {
+    hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+  });
+  andThen(function() {
+    assert.equal(find('.player .assign').size(), 3);
+    assert.equal(find('.player .deduct').size(), 3);
+  });
+
+  click('.player .deduct:eq(0)');
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 1);
+    assert.equal(find('.player .assign').size(), 2);
+    assert.equal(find('.player .deduct').size(), 2);
+  });
+});
+
+test('all buttons disappear after answer is passed', function(assert){
+  assert.expect(5);
+  visit('/host/abc123');
+  var hostController = application.__container__.lookup('controller:host');
+  hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+
+
+  andThen(function() {
+    assert.equal(find('.player .assign').size(), 3);
+    assert.equal(find('.player .deduct').size(), 3);
+  });
+
+  click('.pass');
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 0);
+    assert.equal(find('.player .assign').size(), 0);
+    assert.equal(find('.player .deduct').size(), 0);
+  });
+});
+
+test('all buttons disappear after assignment', function(assert){
+  assert.expect(6);
+  visit('/host/abc123');
+  var hostController = application.__container__.lookup('controller:host');
+
+  var hostRoute = application.__container__.lookup('route:host');
+  hostRoute.reopen({
+    actions: {
+      emit:function(data){
+        assert.ok(data.result.Bob);
+      }
+    }
+  });
+  andThen(function(){
+    hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+  });
+  andThen(function() {
+    assert.equal(find('.player .assign').size(), 3);
+    assert.equal(find('.player .deduct').size(), 3);
+  });
+
+  click('.player .assign:eq(0)');
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 0);
+    assert.equal(find('.player .assign').size(), 0);
+    assert.equal(find('.player .deduct').size(), 0);
+  });
+});
+
+test('buttons reappear upon new clue', function(assert){
+  assert.expect(9);
+  visit('/host/abc123');
+  var hostController = application.__container__.lookup('controller:host');
+  hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 1);
+    assert.equal(find('.player .assign').size(), 3);
+    assert.equal(find('.player .deduct').size(), 3);
+  });
+
+  click('.pass');
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 0);
+    assert.equal(find('.player .assign').size(), 0);
+    assert.equal(find('.player .deduct').size(), 0);
+  });
+
+  andThen(function(){
+    hostController.send('onmessage', {data: "{\"clue\":{\"q\":\"foo\",\"a\":\"bar\"},\"players\": [{\"name\": \"Bob\"}, {\"name\": \"Deb\"}, {\"name\": \"Tim\"}]}"});
+  });
+
+  andThen(function() {
+    assert.equal(find('.pass').size(), 1);
+    assert.equal(find('.player .assign').size(), 3);
+    assert.equal(find('.player .deduct').size(), 3);
   });
 });
